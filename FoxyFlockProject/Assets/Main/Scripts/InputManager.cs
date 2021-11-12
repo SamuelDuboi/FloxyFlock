d75f5820982022e5ce78e5478e42cc45e58d34a5
sam <s.dubois@rubika-edu.com>
2021-11-12 13:55:58 +0100

    vfx graph added

--- FoxyFlockProject/Assets/Main/Scripts/InputManager.cs
+++ FoxyFlockProject/Assets/Main/Scripts/InputManager.cs
@@ -3,7 +3,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.Events;
 public class InputManager : MonoBehaviour
 {
-
+    
     public HandController rightHand;
     public HandController leftHand;
     private bool rightHandIsTrigger;
