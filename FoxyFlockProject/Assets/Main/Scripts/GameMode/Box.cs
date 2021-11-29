00fb0cdbce3424f171f995b676b2cd9c27b31c9c
sam <s.dubois@rubika-edu.com>
2021-11-29 21:47:18 +0100

    trying to fix simon box

--- FoxyFlockProject/Assets/Main/Scripts/GameMode/Box.cs
+++ FoxyFlockProject/Assets/Main/Scripts/GameMode/Box.cs
@@ -6,7 +6,7 @@ public class Box : MonoBehaviour
 {
     public Collider thisBox;
     public List<GameObject> objectsInBox;
-
+    public List<GrabbableObject> grabbableObjects;
     public Material baseMat;
     public Material winMat;
     public Material defeatMat;
@@ -18,6 +18,7 @@ public class Box : MonoBehaviour
     {
         thisBox = gameObject.GetComponent<Collider>();
         center = gameObject.transform.position;
+        grabbableObjects = new List<GrabbableObject>();
     }
 
     private void Update()
@@ -28,17 +29,41 @@ public class Box : MonoBehaviour
 
         }*/
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            GrabbableObject parent = other.GetComponentInParent<GrabbableObject>();
+            if (grabbableObjects.Contains(parent))
+                return;
+            else
+            {
+                for (int i = 0; i < parent.colliders.Count; i++)
+                {
+                    if(!thisBox.bounds.Contains(parent.colliders[i].bounds.max) || !thisBox.bounds.Contains(parent.colliders[i].bounds.max))
+                    {
+                        return;
+                    }
+                }
+                grabbableObjects.Add(parent);
+            }
+        }
+ }
     private void OnTriggerStay(Collider other)
     {
 
-        if (other.gameObject.layer == layerMask)
+        if (other.gameObject.layer == 6)
         {
+          
+
             if (other.bounds.Contains(thisBox.bounds.min) && other.bounds.Contains(thisBox.bounds.max))
             {
                 objectsInBox.Add(other.gameObject);
             }
             else
             {
+                if (objectsInBox.Count < 1)
+                    return;
                 objectsInBox.RemoveAt(objectsInBox.BinarySearch(other.gameObject));
             }
 
