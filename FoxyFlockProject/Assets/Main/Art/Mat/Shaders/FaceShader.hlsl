4ade0e7c277facb6fb07242651c8300cc2f785d8
Aubane <a.rittano@rubika-edu.com>
2021-12-02 23:18:14 +0100

    open mouth wip #2

--- FoxyFlockProject/Assets/Main/Art/Mat/Shaders/FaceShader.hlsl
+++ FoxyFlockProject/Assets/Main/Art/Mat/Shaders/FaceShader.hlsl
@@ -43,9 +43,10 @@ float Bezier(in float2 pos, in float2 A, in float2 B, in float2 C)
 	return sqrt(res);
 }
 
-void Face_float (float2 UV, float2 offsetUV, float4 outlineColor, float4 irisColor, float4 eyesColor, 
-	float eyesDistance, float eyesSize, float eyesShape, float irisPositionX, float irisPositionY, float irisSize, float irisShape, float bossEyed, 
-	float outlineSize, bool mouthOpen, float mouthSize, float mouthWidth, float mouthHeight, float mouthThickness, float smile, 
+void Face_float(float2 UV, float2 offsetUV, float4 outlineColor, float4 irisColor, float4 eyesColor, float4 mouthColor,
+	float eyesDistance, float eyesSize, float eyesShape, float irisPositionX, float irisPositionY, float irisSize, float irisShape, float bossEyed,
+	float outlineSize, bool mouthOpen, float mouthWidth, float mouthHeight, float mouthThickness, float smile,
+	float openMouthSize, float openMouthShapeY,
 	float topEyelid, float bottomEyelid, float rings, float straightEyelid,
 	out float alpha, out float4 Out)
 {
@@ -53,6 +54,7 @@ void Face_float (float2 UV, float2 offsetUV, float4 outlineColor, float4 irisCol
 	float2 eyeUV = float2(abs(UV.x*2.0 - 1.0), UV.y);
 	float2 irisUV = float2(lerp(frac(UV.x * 2.0), abs(UV.x * 2.0 - 1.0), bossEyed), UV.y);
 	float2 mouthUV = eyeUV;
+	float2 openMouthUV = UV;
 
 	eyeUV.y -= 0.5;
 	eyeUV.y *= eyesShape;
@@ -61,7 +63,14 @@ void Face_float (float2 UV, float2 offsetUV, float4 outlineColor, float4 irisCol
 	irisUV.y -= 0.5;
 	irisUV.y *= irisShape;
 	irisUV.y += 0.5;
+
+	openMouthUV.y -= 0.5;
+	openMouthUV.y *= openMouthShapeY;
+	openMouthUV.y += 0.5;
+
 	float4 col = 0.0;
+	float3 colEyes = 0.0;
+	float3 colMouth = 0.0;
 
 	float eyes = step(distance(float2(eyesDistance, 0.5), eyeUV), eyesSize);
 
@@ -75,24 +84,32 @@ void Face_float (float2 UV, float2 offsetUV, float4 outlineColor, float4 irisCol
 		: straightEyelid ? step(distance(float2(eyesDistance, 0.5), eyeUV), eyesSize + outlineSize) * step(eyeUV.y, 1.0 - topEyelid + outlineSize) * step(bottomEyelid - outlineSize, eyeUV.y)
 			: step(distance(float2(eyesDistance, 0.5), eyeUV), eyesSize + outlineSize) * step(eyesSize*2.0-outlineSize, distance(float2(eyesDistance, 0.5 + topEyelid), eyeUV)) * step(eyesSize * 2.0-outlineSize, distance(float2(eyesDistance, 0.5 - bottomEyelid), eyeUV));
 
-	float mouth = mouthOpen ? step(distance(float2(mouthWidth, mouthHeight), mouthUV), mouthSize)
+	float mouth = mouthOpen ? step(distance(float2(0.5, mouthHeight + smile/2), openMouthUV), mouthWidth)
 		: smile ? step(Bezier(UV, float2(0.5 - mouthWidth, mouthHeight), float2(0.5, mouthHeight + smile), float2(0.5 + mouthWidth, mouthHeight)), mouthThickness)
 			: step(Segment(UV, float2(0.5 - mouthWidth, mouthHeight), float2(0.5 + mouthWidth, mouthHeight)), mouthThickness);
 
-	alpha = outlineEyes + mouth;
+	float outlineMouth = mouthOpen ? step(distance(float2(0.5, mouthHeight + smile / 2), openMouthUV), mouthWidth + outlineSize) : mouth;
+
+	alpha = outlineEyes + outlineMouth;
 
 	if (eyes > 0) {
 		if (iris > 0) {
-			col.rgb = irisColor;
+			colEyes = irisColor;
 		}
 		else {
-			col.rgb = eyesColor;
+			colEyes = eyesColor;
 		}
 	}
-	else {
-		col.rgb = outlineColor;
-	}
 
+	if (mouthOpen == true) {
+		if (mouth > 0) {
+			colMouth = mouthColor;
+		}
+	}
+	else { colMouth = outlineColor; }
+
+	col.rgb = lerp(outlineColor, lerp(colEyes, colMouth, mouth), eyes + mouth);
+	
 	//col.a = alpha;
 	Out = col;
 }
