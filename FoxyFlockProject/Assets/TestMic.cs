4f5295df4bfb948343bb1b50b30483de18e636ed
sam <s.dubois@rubika-edu.com>
2022-01-10 18:09:00 +0100

    test sound and font

---
+++ FoxyFlockProject/Assets/TestMic.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMic : MonoBehaviour
+{
+    void Start()
+    {
+        Application.runInBackground = true;
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start(Microphone.devices[1], true, 1000, 44100);
+        audioSource.Play();
+        if (Microphone.IsRecording(Microphone.devices[1]))
+        {
+            while (!(Microphone.GetPosition(Microphone.devices[1]) > 0)){ }
+            Debug.Log("is recording");
+        }
+        else
+        {
+            Debug.Log("is not recording");
+
+        }
+    }
+    private void Update()
+    {
+        
+    }
+}
