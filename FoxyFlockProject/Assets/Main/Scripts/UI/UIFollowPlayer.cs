7d0e886269fb2fc93ee6e0eb6207ec0f4845832c
Paul PARET <paret.paul@orange.fr>
2022-01-19 11:57:31 +0100

    Pause UI not working as expected

--- FoxyFlockProject/Assets/Main/Scripts/UI/UIFollowPlayer.cs
+++ FoxyFlockProject/Assets/Main/Scripts/UI/UIFollowPlayer.cs
@@ -15,14 +15,8 @@ public class UIFollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 followPos = (headeSett.position + headeSett.forward * canvasDistanceToCamera);
-        Vector3 canvasToFollowPos = followPos - m_transform.position;
-
-        if (canvasToFollowPos.magnitude > offsetThreshold)
-        {
-            m_transform.Translate(followPos * followSpeed);
-        }
-
         m_transform.rotation = Quaternion.Euler(m_transform.rotation.eulerAngles.x, headeSett.rotation.eulerAngles.y, m_transform.rotation.eulerAngles.z);
+
+        //m_transform.rotation = Quaternion.Lerp(m_transform.rotation, targetRotation, Time.deltaTime * followSpeed);
     }
 }
