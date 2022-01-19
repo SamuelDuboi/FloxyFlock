f58f803d6495e2fd830ab0a18d86756aa32b8eac
PÄRET Paul <paret.paul@orange.fr>
2022-01-19 10:34:53 +0100

    Pause menu should follow player (testing)

--- FoxyFlockProject/Assets/Main/Scripts/UI/UIFollowPlayer.cs
+++ FoxyFlockProject/Assets/Main/Scripts/UI/UIFollowPlayer.cs
@@ -6,9 +6,23 @@ public class UIFollowPlayer : MonoBehaviour
 {
     public Transform headeSett;
     public Transform m_transform;
+
+    public float canvasDistanceToCamera;
+    public float offsetThreshold;
+    public float followSpeed;
+
+
     // Update is called once per frame
     void Update()
     {
+        Vector3 followPos = (headeSett.position + headeSett.forward * canvasDistanceToCamera);
+        Vector3 canvasToFollowPos = followPos - m_transform.position;
+
+        if (canvasToFollowPos.magnitude > offsetThreshold)
+        {
+            m_transform.Translate(followPos * followSpeed);
+        }
+
         m_transform.rotation = Quaternion.Euler(m_transform.rotation.eulerAngles.x, headeSett.rotation.eulerAngles.y, m_transform.rotation.eulerAngles.z);
     }
 }
