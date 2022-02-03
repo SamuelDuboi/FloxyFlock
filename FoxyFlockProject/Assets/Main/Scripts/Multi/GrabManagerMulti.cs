54f7005087ccb4f3edf5ad76e3c20ef27c841cf1
sam <s.dubois@rubika-edu.com>
2022-02-03 19:59:18 +0100

    fix of one small bug cause  3 hours were wasted for nothing

--- FoxyFlockProject/Assets/Main/Scripts/Multi/GrabManagerMulti.cs
+++ FoxyFlockProject/Assets/Main/Scripts/Multi/GrabManagerMulti.cs
@@ -76,12 +76,7 @@ public class GrabManagerMulti : GrabManager
             for (int x = 0; x < batches[i].pieces.Count; x++)
             {
                 // temp solution for attribution
-                int random = UnityEngine.Random.Range(0, negativeModifiers.Count + positiveModifiers.Count + weightOfBasicInRandom);
                 Modifier _modifier = baseModifier;
-                if (random > 15 && random <= negativeModifiers.Count + weightOfBasicInRandom)
-                    _modifier = negativeModifiers[random - weightOfBasicInRandom-1];
-                else if (random > negativeModifiers.Count + weightOfBasicInRandom)
-                    _modifier = positiveModifiers[random - weightOfBasicInRandom-1 - negativeModifiers.Count];
                 Type type = _modifier.actions.GetType();
                 var _object = GetComponent(type);
                 player.InitBacth(authority,v, i, x, batches, _modifier, _object, basicMats, mainPool, out mainPool);
